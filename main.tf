ef9a82ec9f9df25509ab8e2f74c0799e347d868a
Sean Carolan <scarolan@hashicorp.com>
2019-05-27 06:47:09 -0700

    Initial commit.

---
+++ main.tf
@@ -0,0 +1,29 @@
+
+# Change the name pizzaplanet to your own arcade name. This becomes part of your public URL.
+
+variable "prefix" {
+  default = "pizzaplanet"
+}
+
+# Any docker image that runs an app on port 80 will do
+variable "image" {
+  default = "scarolan/pacman"
+}
+
+resource "azurerm_resource_group" "arcade" {
+  name     = "${var.prefix}-containerapp-demo"
+  location = "${var.location}"
+}
+
+module "web_app_container" {
+  source              = "innovationnorway/web-app-container/azurerm"
+  name                = "${var.prefix}"
+  port                = "80"
+  resource_group_name = "${azurerm_resource_group.arcade.name}"
+  container_type      = "docker"
+  container_image     = "${var.image}"
+}
+
+output "container_app_url" {
+  value = "https://${module.web_app_container.hostname}"
+}
