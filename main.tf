a2aa5d82ded4cafbd6b9e9ddb6e145ab651ade68
Sean Carolan <sean.carolan@hashicorp.com>
2021-09-27 11:08:58 -0500

    Fix variable name.

--- main.tf
+++ main.tf
@@ -7,31 +7,6 @@ terraform {
   }
 }
 
-# Change to your own arcade name. This becomes part of your public URL.
-# Example: http://palacearcade.azurewebsites.net/
-variable "prefix" {
-  default = "palace-arcade"
-}
-
-# Any docker image that runs an app on port 80 will do
-# Try the scarolan/pizzaplanet image for an example change.
-variable "image" {
-  default = "scarolan/palacearcade"
-}
-
-# Choose a location
-variable "location" {
-  default = "centralus"
-}
-
-variable "https_only" {
-  default = "false"
-}
-
-variable "sku_size" {
-  default = "S1"
-}
-
 resource "azurerm_resource_group" "arcade" {
   name     = "${var.prefix}-containerapp-demo"
   location = var.location
